86081db710b0ac67a2f61ec6fa7017771da8ca69
Brett <brett.castellanos@gmail.com>
2019-05-28 09:19:19 -0500

    Update scrape_kgs_games.py

--- data_collection/scrape_kgs_games.py
+++ data_collection/scrape_kgs_games.py
@@ -4,5 +4,33 @@ def main():
     return 0
 
 
+def get_user_page():
+    return 0
+
+
+def get_user_subpages():
+    return 0
+
+
+def process_user_subpage():
+    return 0
+
+
+def clean_game_table():
+    return 0
+
+
+def insert_table_to_db():
+    return 0
+
+
+def insert_row_to_db():
+    return 0
+
+
+def insert_user_to_db():
+    return 0
+
+
 if __name__ == "__main__":
     main()
