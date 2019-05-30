f5c08645ea66131bacb03eedf4ebdf120ca2208f
Brett <brett.castellanos@gmail.com>
2019-05-30 12:46:31 -0500

    Create stats_tests.py

---
+++ stats_tests/stats_tests.py
@@ -0,0 +1,27 @@
+import scipy.stats as stats
+import numpy as np
+
+
+def main():
+
+    return 0
+
+def mean_games_played_test():
+    """
+    Test whether the schools have the same mean games played per user.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+    """
+    alpha = .05
+    beta = .05
+
+    
+
+if __name__ == "__main__":
+    main()
